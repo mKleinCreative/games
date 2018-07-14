7fc2286f4fc0a17b56638bde740a42799c41f461
Michael Klein <miketheklein@gmail.com>
2018-07-14 13:07:23 -0700

    finished up rick clicker

--- RickClicker/src/main.js
+++ RickClicker/src/main.js
@@ -6,18 +6,36 @@ function sync (dom, pos) {
   dom.style.top = `${pos.y}px`;
 }
 
-`${pos.x}px`
-
 const addClicker = () => {
   const pos = {
     x: Math.random() * 500,
     y: Math.random() * 300
   };
   const img = new Image();
-  img.src = 'res/images/rick.png';
+  img.src = '../res/images/rick.png';
   img.style.position = 'absolute';
   img.addEventListener('click', removeClicker, false);
 
   document.querySelector("#board").appendChild(img);
   sync(img, pos);
+}
+
+
+const checkGameOver = () => {
+  document.querySelector('#remain').innerHTML = clickers;
+  if (clickers === 0) {
+    const taken = Math.round((Date.now() - startTime) / 1000);
+    alert(`De-ricked in ${taken} seconds!`);
+  }
+
+}
+
+const removeClicker = (e) => {
+  e.target.parentNode.removeChild(e.target);
+  clickers--;
+  checkGameOver();
+}
+
+for (let i = 0; i < clickers; i++) {
+  addClicker();
 }
